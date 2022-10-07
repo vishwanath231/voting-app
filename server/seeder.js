651afaf6fac9982f169da70660ce06b714256294
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-07 14:13:20 +0530

    seeder for import:data & destroy:data

---
+++ server/seeder.js
@@ -0,0 +1,101 @@
+import dotenv from 'dotenv';
+import color from 'colors';
+import connectDB from './config/db.js';
+import userLoginData from './data/auth/userLogin.js';
+import adminLoginData from './data/auth/adminLogin.js';
+import userInfoData from './data/info/userInfo.js';
+import adminInfoData from './data/info/adminInfo.js';
+import User from './models/auth/userModel.js';
+import Admin from './models/auth/adminModel.js';
+import UserInfo from './models/info/userInfoModel.js';
+import AdminInfo from './models/info/adminInfoModel.js';
+
+// confirguration environment
+dotenv.config()
+
+// connect the database
+connectDB()
+
+
+// insert datas in a database;
+const importData = async () => {
+
+    try {
+
+        // first delete the all collections;
+        await User.deleteMany();
+        await Admin.deleteMany();
+        await UserInfo.deleteMany();
+        await AdminInfo.deleteMany();
+
+        // insert user login data in a database
+        const userLogin = await User.insertMany(userLoginData)
+
+        // insert user information in a databse
+        const userInfoArr = [...userInfoData]
+
+        for (let i = 0; i < userLogin.length; i++) {
+            
+            userInfoArr.push(userInfoData[i].user = userLogin._id)
+        }
+
+        // remove same id in user_info collection
+        userInfoArr.forEach((val) => {
+            if (val.user === val.user) {
+                return userInfoArr.pop()
+            }
+        })
+
+        await UserInfo.insertMany(userInfoArr);
+
+        // insert admin login data in a database
+        const adminLogin = await Admin.insertMany(adminLoginData)
+
+        // insert admin information in a database
+        const adminInfoObj = {...adminInfoData, admin: adminLogin[0]._id}
+
+        await AdminInfo.create(adminInfoObj)
+
+
+        console.log(`Data imported`.bgGreen);
+        
+    } catch (err) {
+
+        console.log(`${err}`.bgRed);
+        process.exit(1)
+        
+    }
+}
+
+
+// destroy data in a database
+const destroyData = async () => {
+
+    try {
+        
+        // delete all the collections of database
+        await User.deleteMany();
+        await Admin.deleteMany();
+        await UserInfo.deleteMany();
+        await AdminInfo.deleteMany();
+
+        console.log(`Data destroyed...!`.bgGreen);
+
+    } catch (err) {
+        
+        console.log(`${err}`.bgRed);
+        process.exit(1)
+    }
+}
+
+
+
+// command line execute this 
+if (process.argv[2] === '-d') {
+    destroyData() // npm run data:destroy
+}else{
+    importData() // npm run data:import
+}
+
+
+
