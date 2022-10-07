ac7d93e9619207f15eb3fb50bafd682d60d6d623
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-07 14:09:13 +0530

    admin info data

---
+++ server/data/info/adminInfo.js
@@ -0,0 +1,22 @@
+
+const adminInfoData = {
+    name: "admin",
+    email: "admin@gmail.com",
+    phone_no: 1234567890,
+    gender: "male",
+    birth_date: "09-03-2001",
+    blood_group: "AB+",
+    parent_name: "Vishwanath",
+    community: "DNC",
+    address: {
+        door_no:"456",
+        street:"Kovil street",
+        taluk: "kovilpatti",
+        post: "kovilpatti",
+        city: "viralimalai",
+        district: "trichy",
+        pincode: 654786
+    }
+}
+
+export default adminInfoData;
