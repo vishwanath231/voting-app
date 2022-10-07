fc360879c4ff5e4be74d0fdbc1afb23d32c62204
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-07 22:10:39 +0530

    admin controller

---
+++ server/controllers/adminController.js
@@ -0,0 +1,17 @@
+import bcrypt from 'bcryptjs';
+import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
+import Admin from '../models/auth/adminModel.js';
+
+
+const adminLogin = asyncHandler( async (req, res) => {
+
+    
+
+})
+
+
+export {
+    adminLogin
+};
+
