a56e957106d8a3baa60a5e3a1bc28e33a1ae8043
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-08 20:24:24 +0530

    user router

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { userLogin, userGet } from '../controllers/userController.js'
-
+import { userProtect } from '../middlewares/authMiddleware.js';
 
 router.route('/login').post(userLogin)
-router.route('/').get(userGet)
+router.route('/').get(userProtect, userGet)
 
 
 export default router;
