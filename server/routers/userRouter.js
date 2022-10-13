028cb7b765b273fecc83d9a2290430cc41df4705
solomon-prabu <solomonprabu002@gmail.com>
2022-10-13 22:52:16 +0530

    user router

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,12 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { userLogin, userGet, userInfoGet, generatePin } from '../controllers/userController.js';
+import { userLogin, generatePin } from '../controllers/userController.js';
 import { userProtect } from '../middlewares/authMiddleware.js';
 
 router.route('/login').post(userLogin)
-router.route('/').get(userProtect, userGet)
-router.route('/info').get(userProtect, userInfoGet)
-router.route('/:userPin').get(userProtect, generatePin)
+router.route('/pin').post(userProtect, generatePin)
 
 
 
