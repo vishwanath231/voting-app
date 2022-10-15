a6e9ff60e6b70073b357bc016e18390f89dd1eb5
solomon-prabu <solomonprabu002@gmail.com>
2022-10-15 18:34:09 +0530

    user generate otp screen

--- client/src/screens/client/auth/UserGenerateOtpScreen.js
+++ client/src/screens/client/auth/UserGenerateOtpScreen.js
@@ -190,7 +190,7 @@ const UserGenerateOtpScreen = ({ generateOtp , userGenerateOTP}) => {
                             </div>
                         </div>
                     }
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Submit</button>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Generate OTP</button>
                 </form>
             </div>
         </main>
