741bf4a5a8e6c489ceca8c35bd8b6c296e3bfe1f
solomon-prabu <solomonprabu002@gmail.com>
2022-10-15 18:27:57 +0530

    auth actions

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -2,15 +2,63 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
-    ADMIN_LOGIN_REQUEST,
-    ADMIN_LOGIN_SUCCESS,
-    ADMIN_LOGIN_FAIL,
     LOGOUT,
     USER_GENERATE_OTP_REQUEST,
     USER_GENERATE_OTP_SUCCESS,
-    USER_GENERATE_OTP_FAIL
+    USER_GENERATE_OTP_FAIL,
+    USER_VERIFY_OTP_REQUEST,
+    USER_VERIFY_OTP_SUCCESS,
+    USER_VERIFY_OTP_FAIL,
+    USER_LOADED_REQUEST,
+    USER_LOADED_FAIL,
+    USER_LOADED_SUCCESS
 } from '../constants/authConstants';
 import axios from 'axios';
+
+
+export const userLoaded = () => async (dispatch, getState) => {
+
+    try {
+        
+        dispatch({
+            type: USER_LOADED_REQUEST
+        })
+
+        const { verifyOtp: { info } } = getState();
+
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization : `Bearer ${info}`
+            }
+        }
+
+        const { data } = await axios.get('http://localhost:5000/api/users/profile', config)
+
+        dispatch({
+            type: USER_LOADED_SUCCESS,
+            payload: data
+        })
+
+
+    } catch (error) {
+
+        const message = error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+        if (message === 'Not authorized, token failed') {
+            dispatch(logout())
+        }
+ 
+        dispatch({
+            type: USER_LOADED_FAIL,
+            payload: message
+        })
+    }
+
+    
+}
 
 
 export const userLogin = (loginData) => async (dispatch) => {
@@ -86,6 +134,7 @@ export const userGenerateOTP = (datas) => async (dispatch, getState) => {
         })
 
         localStorage.setItem('userPin', data.token)
+        
 
     } catch (error) {
 
@@ -97,5 +146,60 @@ export const userGenerateOTP = (datas) => async (dispatch, getState) => {
         })
     }
 
+}
+
+
+
+
+export const userVerifyOTP = (datas) => async (dispatch, getState) => {
+    
+    try {
+        
+        dispatch({
+            type: USER_VERIFY_OTP_REQUEST
+        })
+
+        const { generateOtp: { info } } = getState();
+
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization : `Bearer ${info}`
+            }
+        }
+
+        const { data } = await axios.post('http://localhost:5000/api/users/verify', datas, config)
+
+
+        dispatch({
+            type: USER_VERIFY_OTP_SUCCESS,
+            payload: data.token
+        })
+
+        localStorage.setItem('userVerify', data.token)
+        dispatch(userLoaded())
+
+    } catch (error) {
+
+        const resErr =  error.response && error.response.data.message ? error.response.data.message : error.message
+        
+        dispatch({
+            type: USER_VERIFY_OTP_FAIL,
+            payload: resErr
+        })
+    }
+
+}
+
+
+
+export const logout = () => (dispatch) => {
+
+    dispatch({ type: LOGOUT })
+    localStorage.removeItem('userInfo')
+    localStorage.removeItem('userPin')
+    localStorage.removeItem('userVerify')
+    document.location.href = '/';
 }
 
