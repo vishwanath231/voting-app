ba003e0a4f5938412fe55f9481daab127d00fe99
solomon-prabu <solomonprabu002@gmail.com>
2022-10-15 18:39:08 +0530

    react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { userLoaded } from './redux/actions/authActions';
 import Router from './routes/Router';
+import { connect } from 'react-redux';
 
-const App = () => {
+const App = ({ userLoaded }) => {
+
+    useEffect(() => {
+        userLoaded()
+     },[userLoaded])
+
     return (
         <Router />
     )
 }
 
-export default App;
+export default connect(null, { userLoaded })(App);
