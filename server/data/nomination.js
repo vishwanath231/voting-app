506e702b1d244be531136a8e5cb5a1d37adeeecd
solomon-prabu <solomonprabu002@gmail.com>
2022-10-17 20:58:33 +0530

    Nomination data

---
+++ server/data/nomination.js
@@ -0,0 +1,54 @@
+const nominationData = [
+    {
+        profile: 'https://res.cloudinary.com/ddni4sjyo/image/upload/v1644849197/kk1a2apbrvyljwe3zqrz.jpg',
+        reg_no: '21csea35',
+        name: 'Vishwanath A',
+        email: 'vishwanathvishwabai@gmail.com',
+        phone_no: 6385213119,
+        gender: "male",
+        birth_date: '09-03-2001',
+        parent_name: "Aruchunan",
+        community: "DNC",
+        address: {
+            door_no: '105',
+            street: 'JJ Nagar',
+            city: 'Thuvarankurichy',
+            taluk: 'Marungapuri',
+            post: 'Ikkaraikosikurichy',
+            district: 'Tiruchirappalli',
+            pincode: 621314
+        },
+        party_logo: 'https://res.cloudinary.com/ddni4sjyo/image/upload/v1644849198/ywlyo8bzc4yx4mcgct99.png',
+        party_name: 'pen',
+        profile_cloudinary_id: '1',
+        party_logo_cloudinary_id: '1'
+    },
+    {
+        profile: 'https://res.cloudinary.com/ddni4sjyo/image/upload/v1644848876/rb29j8msvq7waqufqrdy.jpg',
+        reg_no: "21CSEA11",
+        name: "A.HARI THILAK VARMA",
+        email: "harithilak0999@gmail.com",
+        phone_no: 6379489139,
+        gender: "male",
+        birth_date: "27-09-1999",
+        blood_group: "O+",
+        parent_name: "J.ARUL KUMAR",
+        community: "BC",
+        address:{
+            door_no: "9b",
+            street: "Ramakrishna Nagar",
+            city: "Porur",
+            post: "Porur",
+            taluk: "Porur",
+            district:"Chennai",
+            pincode: 600116 
+        },
+        party_logo: 'https://res.cloudinary.com/ddni4sjyo/image/upload/v1645987315/uwuref9gv56xvs1krj3c.png',
+        party_name: 'pencil',
+        profile_cloudinary_id: '2',
+        party_logo_cloudinary_id: '2'
+    }
+]
+
+
+export default nominationData;
