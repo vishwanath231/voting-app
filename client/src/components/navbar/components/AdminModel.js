ffe66a3ee651dcca013d7f16f99b3973bd123316
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-20 22:16:35 +0530

    user model

--- client/src/components/navbar/components/AdminModel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdminModel = () => {
-    return (
-        <div>AdminModel</div>
-    )
-}
-
-export default AdminModel;
--- client/src/components/navbar/components/UserModel.js
+++ client/src/components/navbar/components/UserModel.js
@@ -2,36 +2,41 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logout } from '../../../redux/actions/authActions';
+import Loader from '../../Loader';
 
-const UserModel = ({ profile, logout, user }) => {
+const UserModel = ({ profile, logout, auth }) => {
 
     const logoutHandler = () => {
         logout()
     }
 
-    const { userInfo } = user;
+    const { loading, info } = auth;
     
     return (
         <div className={ profile ? "z-50 my-4 text-base list-none bg-white sen-font rounded divide-y divide-gray-100 shadow showModel__box" : "hidden" }>
-            <div className="py-3 px-4 text-md text-left">
-                <span className="block">{userInfo && userInfo.reg_no}</span> 
-                <span className="block font-semibold my-1">{userInfo && userInfo.name}</span>
-                <span className="block font-semibold truncate">{userInfo && userInfo.email}</span>
-            </div>
-            <ul className="py-1 text-md text-left">
-                <li>
-                    <Link to='/profile' className="block py-2 px-4 hover:bg-gray-200 ">Profile</Link>
-                </li>
-                <li>
-                    <button onClick={logoutHandler} className="block py-2 px-4 w-full text-left hover:bg-rose-600">Sign out</button>
-                </li>
-            </ul>
+            { loading ? <Loader /> : (
+                <>
+                    <div className="py-3 px-4 text-md text-left">
+                        <span className="block">{info && info.reg_no}</span> 
+                        <span className="block font-semibold my-1">{info && info.name}</span>
+                        <span className="block font-semibold truncate">{info && info.email}</span>
+                    </div>
+                    <ul className="py-1 text-md text-left">
+                        <li>
+                            <Link to='/profile' className="block py-2 px-4 hover:bg-gray-200 ">Profile</Link>
+                        </li>
+                        <li>
+                            <button onClick={logoutHandler} className="block py-2 px-4 w-full text-left hover:bg-rose-600">Sign out</button>
+                        </li>
+                    </ul>
+                </>
+            )}
         </div>
     )
 }
 
 const mapStateToProps = (state) => ({
-    user: state.user
+    auth: state.auth
 })
 
 export default connect(mapStateToProps, { logout })(UserModel);
