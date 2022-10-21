42ca58a8b14943383a21fd302bc42c046913d8df
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-21 13:53:26 +0530

    reducer index

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,11 +1,24 @@
 import { combineReducers } from 'redux';
-import { userLoginReducer, adminLoginReducer, userGenerateOtpReducer, userVerifyOtpReducer, authLoadedReducer } from './authReducers';
+import { 
+    nominationListReducer, 
+    userListReducer 
+} from './adminReducers';
+
+import { 
+    userLoginReducer, 
+    adminLoginReducer, 
+    userGenerateOtpReducer, 
+    userVerifyOtpReducer, 
+    authLoadedReducer
+} from './authReducers';
 
 export const reducers = combineReducers({
     userLoginInfo: userLoginReducer,
     adminLoginInfo: adminLoginReducer,
     generateOtp: userGenerateOtpReducer,
     verifyOtp: userVerifyOtpReducer,
-    auth: authLoadedReducer
+    auth: authLoadedReducer,
+    userList: userListReducer,
+    nominationList: nominationListReducer
 })
 
