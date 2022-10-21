be5411e5148af0d91beb9f1c2c7fd812f6a3f2c1
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-21 10:26:58 +0530

    admin router

--- server/routers/adminRouter.js
+++ server/routers/adminRouter.js
@@ -1,9 +1,22 @@
 import express from 'express';
 const router = express.Router();
-import { adminLogin, profile } from '../controllers/adminController.js';
+import { 
+    adminLogin, 
+    profile,
+    getUsersList,
+    getUserById,
+    getNominationList,
+    getNominationById 
+} from '../controllers/adminController.js';
 import { adminProtect } from '../middlewares/authMiddleware.js';
 
 router.route('/login').post(adminLogin)
 router.route('/profile').get(adminProtect, profile)
+router.route('/user/list').get(adminProtect, getUsersList)
+router.route('/user/:id').get(adminProtect, getUserById)
+router.route('/nomination/list').get(adminProtect, getNominationList)
+router.route('/nomination/:id').get(adminProtect, getNominationById)
+
+
 
 export default router;
