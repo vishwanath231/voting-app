4870b16370308e820e289d32aded2eed47815914
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-21 13:58:44 +0530

    nomination details screen

---
+++ client/src/screens/admin/AdminSingleNominationDetailsScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminSingleNominationDetailsScreen = () => {
+    return (
+        <div>AdminSingleNominationDetailsScreen</div>
+    )
+}
+
+export default AdminSingleNominationDetailsScreen;
