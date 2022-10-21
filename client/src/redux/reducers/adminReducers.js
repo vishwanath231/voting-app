4e95f54c9c4272dca1b4020e76047b8d9d19b8dc
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-21 22:03:22 +0530

    admin reducer

--- client/src/redux/reducers/adminReducers.js
+++ client/src/redux/reducers/adminReducers.js
@@ -6,7 +6,13 @@ import {
     NOMINATION_LIST_SUCCESS,
     NOMINATION_LIST_FAIL,
     USER_LIST_RESET,
-    NOMINATION_LIST_RESET
+    NOMINATION_LIST_RESET,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+    NOMINATION_DETAILS_REQUEST,
+    NOMINATION_DETAILS_SUCCESS,
+    NOMINATION_DETAILS_FAIL
 } from '../constants/adminConstants';
 
 
@@ -42,6 +48,33 @@ export const userListReducer = (state = { users: [] }, { type, payload }) => {
 }
 
 
+export const userDetailsReducer = (state = { user: {} }, { type, payload }) => {
+
+    switch (type) {
+        case USER_DETAILS_REQUEST:
+            return {
+                loading: true,
+                user: {}
+            }
+        
+        case USER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                user: payload
+            }
+
+        case USER_DETAILS_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+
 
 
 export const nominationListReducer = (state = { nominations: [] }, { type, payload }) => {
@@ -68,6 +101,35 @@ export const nominationListReducer = (state = { nominations: [] }, { type, paylo
         case NOMINATION_LIST_RESET:
             return { 
                 nominations: [] 
+            }
+
+        default:
+            return state;
+    }
+}
+
+
+
+
+export const nominationDetailsReducer = (state = { nomination: {} }, { type, payload }) => {
+
+    switch (type) {
+        case NOMINATION_DETAILS_REQUEST:
+            return {
+                loading: true,
+                nomination: {}
+            }
+        
+        case NOMINATION_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                nomination: payload
+            }
+
+        case NOMINATION_DETAILS_FAIL:
+            return {
+                loading: false,
+                error: payload
             }
 
         default:
