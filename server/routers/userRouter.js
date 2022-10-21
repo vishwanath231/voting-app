2c472c0281dbcbffafbb327041fc41d5c328189f
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-21 10:27:19 +0530

    user router

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,12 +1,22 @@
 import express from 'express';
 const router = express.Router();
-import { userLogin, generatePin, verfiyPin, userProfile } from '../controllers/userController.js';
+import { 
+    userLogin, 
+    generatePin, 
+    verfiyPin, 
+    userProfile,
+    getNominationList,
+    getNominationById 
+} from '../controllers/userController.js';
 import { userProtect } from '../middlewares/authMiddleware.js';
 
 router.route('/login').post(userLogin)
 router.route('/pin').post(userProtect, generatePin)
 router.route('/verify').post(userProtect, verfiyPin)
 router.route('/profile').get(userProtect, userProfile)
+router.route('/nomination').get(userProtect, getNominationList)
+router.route('/nomination/:id').get(userProtect, getNominationById)
+
 
 
 
