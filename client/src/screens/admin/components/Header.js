aeec3d93e46c47e1afa59a327e804a21b0640085
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-23 14:51:43 +0530

    header

--- client/src/screens/admin/components/Header.js
+++ client/src/screens/admin/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <div className='md:ml-72 dashboard__header md:mr-2 shadow md:mb-4 p-4 bg-white'>
             <div className='flex justify-between items-center'>
-                <Link to='/admin/dashboard' className='text-2xl font-semibold mont-font tracking-wide'>Dashboard</Link>
+                <Link to='/admin/dashboard' className='text-2xl font-semibold sen-font tracking-wide'>Dashboard</Link>
                 <ModelBox />
             </div>
         </div>
