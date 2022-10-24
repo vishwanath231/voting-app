72c0dd245ed63181ab0e1a70954dfcd2e93b2ea8
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-24 22:08:38 +0530

    nomination list screen

---
+++ client/src/screens/client/NominationListScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+
+const NominationListScreen = () => {
+    return (
+        <>
+            <Navbar />
+            <div className='pt-28 pb-10 bg-gray-100 min-h-screen'>
+                <div>NominationListScreen</div>
+            </div>
+        </>
+    )
+}
+
+export default NominationListScreen;
