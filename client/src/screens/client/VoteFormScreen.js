040b28b79b145852d119ee4029acc13a4a02a56c
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-10-24 22:08:12 +0530

    Vote form screen

---
+++ client/src/screens/client/VoteFormScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+
+const VoteFormScreen = () => {
+    return (
+        <>
+            <Navbar />
+            <div className='pt-28 pb-10 bg-gray-100 min-h-screen'>
+                <div>VoteFormScreen</div>
+            </div>
+        </>
+    )
+}
+
+export default VoteFormScreen;
