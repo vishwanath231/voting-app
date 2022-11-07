00b33f9d876a4fc9324e7c6b85e0161fce7a896d
solomon-prabu <solomonprabu002@gmail.com>
2022-11-08 03:32:03 +0530

    vote schema

--- server/models/voteModel.js
+++ server/models/voteModel.js
@@ -14,6 +14,10 @@ const votingSchema = new Schema({
        type: String,
        required: true
     },
+    community: {
+        type: String,
+        required: true
+    },
     location:{
        type: String,
        required: true
