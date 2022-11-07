8d68c64bfd110d31ef404b5372b868240a38ba3e
solomon-prabu <solomonprabu002@gmail.com>
2022-11-08 03:29:34 +0530

    admin info data

--- server/data/info/adminInfo.js
+++ server/data/info/adminInfo.js
@@ -2,20 +2,16 @@
 const adminInfoData = {
     name: "admin",
     email: "admin@gmail.com",
-    phone_no: 1234567890,
+    phone_no: 6385213119,
     gender: "male",
     birth_date: "09-03-2001",
-    blood_group: "AB+",
-    parent_name: "Vishwanath",
     community: "DNC",
     address: {
-        door_no:"456",
-        street:"Kovil street",
-        taluk: "kovilpatti",
-        post: "kovilpatti",
-        city: "viralimalai",
-        district: "trichy",
-        pincode: 654786
+        taluk: "Marungapuri",
+        post: "Ikkaraikosikurichi",
+        city: "thuvarankurichi",
+        district: "Tiruchirappalli",
+        pincode: 621314
     }
 }
 
